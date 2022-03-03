4dfb4157a1a939c3308225c2014d6514861dab56
Sardarius <sardariuss@gmail.com>
2022-03-02 22:46:02 -0500

    First commit

---
+++ canisters/game/src/lib.rs
@@ -0,0 +1,121 @@
+use ic_cdk_macros;
+use ic_cdk::export::{candid::{CandidType}, Principal};
+
+#[ic_cdk_macros::import(canister = "graphql")]
+struct GraphQLCanister;
+
+use serde::{Deserialize};
+use serde_json::{Result, Value};
+
+#[derive(Debug, serde::Deserialize, ic_cdk::export::candid::CandidType, Clone)]
+struct Question {
+   id: String,
+   author: User,
+   text: String,
+   interest: i32
+}
+
+#[derive(Debug, serde::Deserialize, ic_cdk::export::candid::CandidType, Clone)]
+struct User {
+   id: String,
+   username: String
+}
+
+// @todo
+#[ic_cdk_macros::update]
+async fn add_user(username: String) -> User {
+   let query = String::from(r#"mutation ($username: String!) {
+      createUser(input: {username: $username}) {
+        id
+        username
+      }
+    }"#);
+   let params = format!(r#"{{"username": "{}"}}"#, username);
+   let json_str = GraphQLCanister::graphql_mutation(
+      query, params).await;
+   let json_data : Value = serde_json::from_str(&json_str.0).unwrap();
+   let user : User = serde_json::from_value(json_data["data"]["createUser"][0].clone()).unwrap();
+   return user;
+}
+
+// @todo
+#[ic_cdk_macros::query]
+async fn get_user(username: String) -> User {
+   let query = r#"query($username:String!){
+     readUser(search:{username:{eq:$username}}){
+       id
+       username
+     }
+   }"#.to_string();
+   let params = format!(r#"{{"username": "{}"}}"#, username);
+   let json_str = GraphQLCanister::graphql_query(
+      query, params).await;
+   let json_data : Value = serde_json::from_str(&json_str.0).unwrap();
+   let user : User = serde_json::from_value(json_data["data"]["readUser"][0].clone()).unwrap();
+   return user;
+}
+
+//// @todo
+//#[ic_cdk_macros::query]
+//async fn get_users() -> [User] {
+//   let query = r#"query(){
+//     readUser(){
+//       id
+//       username
+//     }
+//   }"#.to_string();
+//   let params = format!(r#"{{}}"#);
+//   let json_str = GraphQLCanister::graphql_query(
+//      query, params).await;
+//   let json_data : Value = serde_json::from_str(&json_str.0).unwrap();
+//   let users : [User] = serde_json::from_value(json_data["data"]["readUser"].clone()).unwrap();
+//   return users;
+//}
+
+// @todo
+#[ic_cdk_macros::update]
+async fn add_question(author_id: String, text: String) -> Question{
+   let query = r#"mutation($author_id: ID!, $text: String!) {
+      createQuestion(
+        input: {author: {connect: $author_id}, text: $text, interest: 0}
+      ) {
+        id
+        author {id username}
+        text
+        interest
+      }
+    }
+    "#.to_string();
+   println!("Query: {}", query);
+   
+   let params = format!(r#"{{
+      "author_id": "{}",
+      "text": "{}"
+    }}"#, author_id, text);
+    println!("Params: {}", params);
+
+   let json_str = GraphQLCanister::graphql_mutation(
+      query, params).await;
+   let json_data : Value = serde_json::from_str(&json_str.0).unwrap();
+   let question : Question = serde_json::from_value(json_data["data"]["createQuestion"][0].clone()).unwrap();
+   return question;
+}
+
+// @todo
+#[ic_cdk_macros::query]
+async fn get_question(question_id: String) -> Question {
+   let query = r#"query($question_id:ID!){
+      readQuestion(search:{id:{eq:$question_id}}){
+        id
+        author {id username}
+        text
+        interest
+      }
+    }"#.to_string();
+   let params = format!(r#"{{"question_id": "{}"}}"#, question_id);
+   let json_str = GraphQLCanister::graphql_query(
+      query, params).await;
+   let json_data : Value = serde_json::from_str(&json_str.0).unwrap();
+   let question : Question = serde_json::from_value(json_data["data"]["readQuestion"][0].clone()).unwrap();
+   return question;
+}
