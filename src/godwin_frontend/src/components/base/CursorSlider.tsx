54f87871cf89ee7a95b41809c57f39e7b0badaad
sardariuss <sardariuss@gmail.com>
2023-07-31 19:02:33 -0400

    Fix categories banner on chrome

--- src/godwin_frontend/src/components/base/CursorSlider.tsx
+++ src/godwin_frontend/src/components/base/CursorSlider.tsx
@@ -1,6 +1,6 @@
 import { PolarizationInfo, toCursorInfo } from '../../utils';
 
-import { useState, useEffect }            from "react";
+import React, { useState, useEffect }     from "react";
 
 type Props = {
   id: string;
